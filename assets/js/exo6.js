d091938efaf1d51f92811838515419ade6c9e154
thorleif02 <douezflorian@gmail.com>
2018-10-09 14:25:09 +0200

    exo6

---
+++ assets/js/exo6.js
@@ -0,0 +1,7 @@
+function changeImages(image){
+  document.getElementById(image).src='assets/images/'+image+'_2.jpg';
+}
+function originImages(image){
+  document.getElementById(image).src='assets/images/'+image+'.jpg';
+}
+ 
